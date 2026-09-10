eb3631c43f7458405c65543d99348d216233be39
berandalstephanie490-cyber <berandalstephanie490@gmail.com>
2026-09-10 15:48:05 +0800

    commit 9

---
+++ PHP2/INCLUDES/index.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container">
+    <h1>Welcome!</h1>
+    <p style="text-align: center;">This is PHP Output 2</p>
+</div>
+
+<?php include 'includes/footer.php'; ?>
---
+++ PHP2/footer.php
@@ -0,0 +1,8 @@
+</main>
+
+<footer>
+    <p style="background-color: black; padding: 10px; color: white;">&copy; 2026 Php Output #2</p>
+</footer>
+
+</body>
+</html>
